bbf15d9662c85994a4c28dfdda5def2d8dc0a800
Yanis Bennadij <yanis.bennadji@laplateforme.io>
2024-09-20 17:41:46 +0200

    test

---
+++ Jour04/Job01/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Job 01</title>
+</head>
+<body>
+
+    <main>
+        <button id="button">
+            Fetch
+        </button>
+    </main>    
+
+</body>
+</html>
